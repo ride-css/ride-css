23d2f2fe77f26905371f1e13de3fe259fa59fe47
OctoD <iamoctod@gmail.com>
2016-02-11 14:50:00 +0100

    better testing script

--- test/test.js
+++ test/test.js
@@ -1,38 +1,62 @@
-// thanks to TJ of visionmedia. Thanks for making stylus, really.
+// thanks to TJ. Thanks for making stylus, really.
 
 // getting modules
 var fs = require('fs');
+var fsHelp = require('./fs-help');
+var path = require('path');
 var stylus = require('stylus');
 var rideCss = require('../');
 
+var files = []
+var filesFolder = path.join(process.cwd(), './test/cases');
+// return
+fsHelp.utils.walkSync(filesFolder, files);
+
 // Read test/cases directory and filter all `.styl` files, then remove
 // this extension for each file in the collection and prepare to test.
-var cases = fs.readdirSync('test/cases').filter(function (file) {
+var cases = {};
+
+files.filter(function (file) {
   // bitwise flip to treat result as truthy.
   return ~file.indexOf('.styl');
-}).map(function(file){
-  return file.replace('.styl', '');
+}).forEach(function(file){
+  var css = path.join(__dirname, file.replace('.styl', '.css'));
+  var filename = file.replace('.styl', '');
+  var styl = path.join(__dirname, file);
+  var group = path.dirname(file.split('cases/')[1]).replace('.', 'uncategorized');
+
+  cases[group] = cases[group] || []
+  cases[group].push({
+    css: fs.readFileSync(css, 'utf8').replace(/\r/g, '').replace(/\s+/g, ''),
+    group: group,
+    name: filename,
+    styl: fs.readFileSync(styl, 'utf8').replace(/\r/g, '')
+  })
 });
 
 // For each `.styl` and `.css` pair in `test/cases`, compile stylus to css
 // and compare actual result to expected css.
 describe('Mixin integration', function () {
   // iterating cases
-  cases.forEach(function (test) {
-    var name = test.replace(/[-.]/g, ' ');
-    // testing
-    it(name, function () {
-      var path = 'test/cases/'+ test +'.styl';
-      var styl = fs.readFileSync(path, 'utf8').replace(/\r/g, '');
-      var css = fs.readFileSync('test/cases/'+ test +'.css', 'utf8').replace(/\r/g, '').trim();
-      var style = stylus(styl).use(rideCss()).set('filename', path).define('url', stylus.url());
+  Object.keys(cases).forEach(function (group) {
 
-      if (~test.indexOf('compress')) style.set('compress', true);
+    describe(group, function () {
 
-      style.render(function (error, actual) {
-        if (error) throw error;
-        actual.trim().should.equal(css);
+      cases[group].forEach(function (test) {
+        var name = test.name.replace(/\/+/g, ':').replace('cases:', '').replace(/[-.]/g, ' ');
+        // testing
+        it(name, function () {
+          var style = stylus(test.styl).use(rideCss()).set('filename', test.name).define('url', stylus.url());
+
+          if (~test.styl.indexOf('compress')) style.set('compress', true);
+
+          style.render(function (error, actual) {
+            if (error) throw error;
+            actual.replace(/\s+/g, '').trim().should.equal(test.css);
+          });
+        });
       });
+
     });
   });
 });
